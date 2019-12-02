f61cf74fa7a33d804873af301f18f3c0f978e6f7
BGilz7 <B-Gil@msn.com>
2019-12-02 16:40:45 -0500

    Files set up

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app, db
+
+from flask import render_template
+
+from app.models import User
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+
+# define the root/base of this project folder
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
